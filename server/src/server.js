76e1464191ea77bee3d76219a7756e38f6f56080
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-01 11:29:21 +0300

    connect database with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require("http");
-const app = require("./app.js");
-const { loadPlanetsData } = require("./models/planets.model.js");
+const mongoose = require("mongoose");
 
-const server = http.createServer(app);
+const app = require("./app.js");
+
+const { loadPlanetsData } = require("./models/planets.model.js");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://omnimath:u96wnHORmX0zkLSY@nasa.m2i4sfz.mongodb.net/?retryWrites=true&w=majority"
+
+const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection is ready!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("Error", error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL); 
   await loadPlanetsData();
 
   server.listen(PORT, () => {
