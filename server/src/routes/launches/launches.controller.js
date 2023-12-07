2b470a7c9a4991fb7b9d4cbabac5878d20466078
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-07 16:11:02 +0300

    add launch delete request and error handling delete launch by its flight number and return status of launch abort

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -11,7 +11,7 @@ async function httpGetAllLaunches(req, res) {
 
 async function httpAddNewLaunch(req, res) {
     const launch = req.body;
-    if (
+    if (    
         !launch.mission ||
         !launch.rocket ||
         !launch.launchDate ||
@@ -35,17 +35,27 @@ async function httpAddNewLaunch(req, res) {
     return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res){
+async function httpAbortLaunch(req, res){
     const launchId = Number(req.params.id);
 
-    if(!existsLaunchWithId(launchId)){
+    const existsLaunch = await existsLaunchWithId(launchId);
+    if(!existsLaunch){
         return res.status(404).json({
             error: "Launch not found",
         });
     }
 
-    const aborted = abortedLauchById(launchId);
-    return res.status(200).json(aborted);   
+    const aborted = await abortedLauchById(launchId);
+    
+    if(!aborted){
+      return res.status(400).json({
+        error: "Launch not aborted",
+      })
+    }
+
+    return res.status(200).json({
+      ok: true,
+    });   
 }
 
 
