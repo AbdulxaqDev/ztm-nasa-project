bc87bdb4adb75bc4ba3d1193e4e2753bbebc47d4
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-08 09:49:25 +0300

    seperate connection to database as a module seperated the mongo connection, because, need to connect data base while testing. So, database connection can be triggered from any module in server.

---
+++ server/src/services/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+
+const MONGO_URL = "mongodb+srv://omnimath:u96wnHORmX0zkLSY@nasa.m2i4sfz.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection is ready!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("Error", error);
+});
+
+async function mongoConnect(){
+  await mongoose.connect(MONGO_URL); 
+}
+
+
+
+module.exports = {
+  mongoConnect,
+}
