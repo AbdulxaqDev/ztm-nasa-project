f0f671a8d090c757d68524dc1a1f4332cc02ed34
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-12 17:03:21 +0300

    load launches data

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const { mongoConnect } = require("./services/mongo.js");
 const app = require("./app.js");
 const { loadPlanetsData } = require("./models/planets.model.js");
+const { loadLaunchesData } = require("./models/launches.model.js");
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,7 +12,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect(); 
   await loadPlanetsData();
-
+  await loadLaunchesData();
   server.listen(PORT, () => {
     console.log("Server is running on port", PORT);
   });
